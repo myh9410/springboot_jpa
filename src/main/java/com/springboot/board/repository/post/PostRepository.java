97a2388b2537b4251edaaa25d6f7dfd12480e066
myh9410 <mjskks94@naver.com>
2023-02-27 17:32:04 +0900

    queryhint를 통한 timeout 설정

--- src/main/java/com/springboot/board/repository/post/PostRepository.java
+++ src/main/java/com/springboot/board/repository/post/PostRepository.java
@@ -2,15 +2,20 @@ package com.springboot.board.repository.post;
 
 import com.springboot.board.entity.Posts;
 import jakarta.persistence.LockModeType;
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.data.repository.query.Param;
 
 public interface PostRepository extends JpaRepository<Posts, Long> {
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("select p from Posts p where p.no = :id")
+    @Query("select p from posts p where p.no = :id")
+    @QueryHints({
+        @QueryHint(name = "jakarta.persistence.lock.timeout", value = "3000")
+    })
     Posts findByIdForUpdate(@Param("id") long id);
 
 }
