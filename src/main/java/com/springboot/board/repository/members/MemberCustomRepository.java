034cd828a6305287463e28fa31c3a35a5356fa43
myh9410 <mjskks94@naver.com>
2023-03-09 17:24:32 +0900

    paging 테스트용 추가

--- src/main/java/com/springboot/board/repository/members/MemberCustomRepository.java
+++ src/main/java/com/springboot/board/repository/members/MemberCustomRepository.java
@@ -2,10 +2,13 @@ package com.springboot.board.repository.members;
 
 import com.springboot.board.dto.MemberDto;
 import com.springboot.board.entity.Members;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface MemberCustomRepository {
 
     List<MemberDto> findMemberByNo(long no);
+
+    List<MemberDto> findMemberByNoWithPaging(long no, Pageable pageable);
 }
--- src/main/java/com/springboot/board/repository/members/MemberCustomRepositoryImpl.java
+++ src/main/java/com/springboot/board/repository/members/MemberCustomRepositoryImpl.java
@@ -1,20 +1,25 @@
 package com.springboot.board.repository.members;
 
+import com.querydsl.core.types.Order;
+import com.querydsl.core.types.OrderSpecifier;
 import com.querydsl.core.types.Projections;
 import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.springboot.board.dto.MemberDto;
 import com.springboot.board.dto.OrderDto;
-import com.springboot.board.dto.QMemberDto;
-import com.springboot.board.entity.Members;
+import com.springboot.board.entity.QMembers;
 import jakarta.persistence.EntityManager;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.querydsl.core.group.GroupBy.groupBy;
 import static com.querydsl.core.group.GroupBy.list;
 import static com.springboot.board.entity.QMembers.members;
 import static com.springboot.board.entity.QOrders.orders;
+import static com.springboot.board.config.persistence.QueryDslUtil.*;
 
 public class MemberCustomRepositoryImpl implements MemberCustomRepository{
 
@@ -49,5 +54,64 @@ public class MemberCustomRepositoryImpl implements MemberCustomRepository{
                         )
                     )
                 );
+    }
+
+    @Override
+    public List<MemberDto> findMemberByNoWithPaging(long no, Pageable pageable) {
+
+        List<OrderSpecifier> ORDERS = getAllOrderSpecifiersOfMember(pageable);
+
+        return queryFactory
+                .from(members)
+                .leftJoin(members.orders, orders)
+                .where(members.no.eq(no))
+                .orderBy(ORDERS.toArray(OrderSpecifier[]::new))
+                .transform(
+                        groupBy(members.no).list(
+                                Projections.fields(
+                                        MemberDto.class,
+                                        members.no,
+                                        members.userId,
+                                        members.joinDate,
+                                        members.active,
+                                        list(Projections.fields(
+                                                OrderDto.class,
+                                                orders.no,
+                                                orders.orderNum,
+                                                orders.orderDate,
+                                                orders.status
+                                        )).as("orders")
+                                )
+                        )
+                );
+    }
+
+
+    private List<OrderSpecifier> getAllOrderSpecifiersOfMember(Pageable pageable) {
+        List<OrderSpecifier> ORDERS = new ArrayList<>();
+
+        if (!pageable.getSort().isEmpty()) {
+            for (Sort.Order order : pageable.getSort()) {
+                Order direction = order.getDirection().isAscending() ? Order.ASC : Order.DESC;
+
+                switch (order.getProperty()) {
+                    case "no":
+                        ORDERS.add(getSortedColumn(direction, QMembers.members, "no"));
+                        break;
+
+                    case "user_id":
+                        ORDERS.add(getSortedColumn(direction, QMembers.members, "user_id"));
+                        break;
+
+                    case "join_date":
+                        ORDERS.add(getSortedColumn(direction, QMembers.members, "join_date"));
+
+                    default:
+                        break;
+                }
+            }
+        }
+
+        return ORDERS;
     }
 }
